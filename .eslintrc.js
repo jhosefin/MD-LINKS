f1b071a39a94ad4edecd21211e75e9b2bd7c4a98
jhosefin <90260920+jhosefin@users.noreply.github.com>
2023-06-04 17:38:35 -0400

    configurando entorno

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,24 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es2021: true,
     node: true,
   },
-  extends: "airbnb-base",
+  extends: [
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:jest/recommended",
+  ],
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 8,
     sourceType: "module",
   },
+  plugins: [
+    "jest",
+  ],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "windows"],
